7ce262b3d09824d2cca4a1cb48475ce7d14f5e32
Mahammad Zeynalov <magamed9r2.2011@gmail.com>
2024-11-16 02:08:50 +0700

    add vercel config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
+// IMP START - Bundler Issues
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      crypto: "empty-module",
+    },
+  },
+  define: {
+    global: "globalThis",
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-router-dom", "react-dom"],
+          "wagmi-vendor": ["wagmi", "viem"],
+        },
+      },
+    },
+  },
+  // IMP END - Bundler Issues
+});
